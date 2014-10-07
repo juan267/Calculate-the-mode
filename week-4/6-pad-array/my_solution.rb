d2b76694785da3497f1fab85985cd1764c31985c
juan267 <j.gomez267@gmail.com>
2014-10-07 18:04:16 -0500

    solution challenge 6

--- week-4/6-pad-array/my_solution.rb
+++ week-4/6-pad-array/my_solution.rb
@@ -5,32 +5,62 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.
 
-# I worked on this challenge with: NAME.
+# I worked on this challenge with: Liorr Morrison.
 
 # 0. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
+# What is the input? Array of integers
+# What is the output? (i.e. What should the code return?) An updated array with the length specified, filled with either nil (default) or a value given by the user (an optional argument)
 # What are the steps needed to solve the problem?
+
 
 
 # 1. Initial Solution
 
 class Array
 
-  def pad
-    #code goes here
+  def pad (min_size, value = nil)
+     copy = self.clone
+    if copy.length >= min_size
+      return copy
+    else
+      until copy.length == min_size
+        copy.push(value)
+      end
+      copy
+    end
   end
 
-  def pad!
-    #code goes here
+    def pad! (min_size, value = nil)
+    if self.length >= min_size
+      return self
+    else
+      until self.length == min_size
+        self.push(value)
+      end
+      self
+    end
   end
 
 end
 
 
+
+
 # 3. Refactored Solution
+
+#I think we have a very nice an elegant solution so nothing to Refactored about it.
+
+
 
 
 
 # 4. Reflection
+
+This was a very fun challenge to work on, i had never work with classes before but my pair did had
+experience with them so he teach me how self work inside the class method. once we had that work out,
+we quickly realized that maing changes on self will be the destructive method, so we needed to create a
+copy of self in order to do the non destructive pad method. After that thigs where very simple we, just
+had to check self length and return it if it was greater that the argument of pad, and if
+it was lower that the argument we push the value until length was equal to the argument
+and that was all.
