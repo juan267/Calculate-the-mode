7e1839905f96214f55de900614be664969485d23
juan267 <j.gomez267@gmail.com>
2014-10-08 17:45:44 -0500

    solution for challenge 5

--- week-4/5-nums-commas-solo-challenge/my_solution.rb
+++ week-4/5-nums-commas-solo-challenge/my_solution.rb
@@ -7,19 +7,90 @@
 
 # 0. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
+# What is the input? an integer
+# What is the output? (i.e. What should the code return?): a String containing the integer separeted by commas
 # What are the steps needed to solve the problem?
+# => define a method
+# => convert the integer to string
+# => get the length of the string
+# => find what happens when you divide the lenght in 3 and what is the remainder for diferent numbers
+# => create a block inside each case of the remainder that insert the commas the number of times that it needs to starting from position -4 and adding -4 to each ne position
 
 
-# 1. Initial Solution
+
+
+# # 1. Initial Solution
+
+# def separate_comma (n)
+#   n = n.to_s
+#   pos = -4
+#   n_lenght= n.length
+#   sec = n_lenght/3
+#   if (n_lenght%3 ==0)
+#     times = sec -1
+#     counter = 1
+#     while counter <= times
+#       n.insert(pos,",")
+#       pos += -4
+#       counter += 1
+#     end
+#   elsif (n_lenght%3 ==1)
+#     counter = 0
+#     while counter < sec
+#       n.insert(pos,",")
+#       pos += -4
+#       counter += 1
+#     end
+#   elsif (n_lenght%3 ==2)
+#     counter = 0
+#     while counter < sec
+#       n.insert(pos,",")
+#       pos += -4
+#       counter += 1
+#     end
+#   end
+#   p n
+# end
+
+
+
+
 
 
 
 
 # 2. Refactored Solution
 
+def separate_comma (n)
+  n = n.to_s
+  pos = -4
+  n_lenght= n.length
+  sec = n_lenght/3
+  if (n_lenght%3 ==0)
+    times = sec -1
+    counter = 1
+    while counter <= times
+      n.insert(pos,",")
+      pos += -4
+      counter += 1
+    end
+  else
+    counter = 0
+    while counter < sec
+      n.insert(pos,",")
+      pos += -4
+      counter += 1
+    end
+  end
+  p n
+end
 
+p separate_comma(10000000000)
 
 
 # 3. Reflection
+
+# This was a fun challenging problem, i try to do it whit the least amount of methods as posible and i think i got a good but
+# dificult to read solution. With this problem i learn that the amount of ways that you can solve a problem are almost infinite
+# and that you can approach a challenge in every way that you want while it makes sense. It also force me to break the problem
+# in small parts and solve one by one.
--- week-4/6-pad-array/my_solution.rb
+++ week-4/6-pad-array/my_solution.rb
@@ -12,6 +12,14 @@
 # What is the input? Array of integers
 # What is the output? (i.e. What should the code return?) An updated array with the length specified, filled with either nil (default) or a value given by the user (an optional argument)
 # What are the steps needed to solve the problem?
+#For the pad method:
+# => def pad that accepts two arguments (min_size=minimun lenght of the array and value=nil as default and anything if specified)
+# => make a copy of self
+# => return the copy if the min:size is less or equal to copy length
+# => else push into copy the value argument until copy lenght equal min_size
+# => return copy
+
+# =>for the destructive pad! method is the same steps but without making the copy and working everything over self.
 
 
 
@@ -49,7 +57,7 @@ end
 
 # 3. Refactored Solution
 
-#I think we have a very nice an elegant solution so nothing to Refactored about it.
+#I think we have a very nice and elegant solution so nothing to Refactored about it.
 
 
 
@@ -57,10 +65,10 @@ end
 
 # 4. Reflection
 
-This was a very fun challenge to work on, i had never work with classes before but my pair did had
-experience with them so he teach me how self work inside the class method. once we had that work out,
-we quickly realized that maing changes on self will be the destructive method, so we needed to create a
-copy of self in order to do the non destructive pad method. After that thigs where very simple we, just
-had to check self length and return it if it was greater that the argument of pad, and if
-it was lower that the argument we push the value until length was equal to the argument
-and that was all.
+# This was a very fun challenge to work on, i had never work with classes before but my pair did had
+# experience with them so he teach me how self work inside the class method. once we had that work out,
+# we quickly realized that maing changes on self will be the destructive method, so we needed to create a
+# copy of self in order to do the non destructive pad method. After that thigs where very simple we, just
+# had to check self length and return it if it was greater that the argument of pad, and if
+# it was lower that the argument we push the value until length was equal to the argument
+# and that was all.
