4f5f7722162378be4161f28185fb8d9913a2bca0
German Hernandez <muygrafico@gmail.com>
2015-04-06 10:04:42 -0500

    firt exercise done

--- Ruby-Algorithms/1-add-it-up/my_solution.rb
+++ Ruby-Algorithms/1-add-it-up/my_solution.rb
@@ -1,1 +1,18 @@
-my_solution.rb
+
+# First 'total' method will receive an array.
+# Then,runing within the array takes each value and adds it to a final result.
+# When it finish to run trough the array, return the final value with the sum of all the vulues
+
+def total(array)
+  result = 0
+  array.each do |item|
+    result += item
+  end
+  result
+end
+
+# ------ Testing ------
+
+total([1,2,3])      # => 6
+total([4.5, 0, -1]) # => 3.5
+total([-100, 100])  # => 0
