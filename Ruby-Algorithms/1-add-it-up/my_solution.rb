f9473abb92cbbbdf055718616fdc4255ce1559cc
Hugo Espinosa <hespino1@eafit.edu.co>
2015-04-06 10:31:35 -0500

    exercise 1

--- Ruby-Algorithms/1-add-it-up/my_solution.rb
+++ Ruby-Algorithms/1-add-it-up/my_solution.rb
@@ -1,1 +1,16 @@
-my_solution.rb
+def sum_array (array)
+    Integer total = 0
+    array.each do |element|
+        total += element
+    end
+    return total
+end
+
+sum_array([1,2,3,4,5])
+
+def sentence_maker (array)
+  array.join(" ")
+end
+
+sentence_maker(["i", "want", "to", "go", "to", "the", "movies"])
+end
